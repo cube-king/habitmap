0ff02d07b170dbe26b9cf5e8fc0f86b692526776
cube-king <100381926+cube-king@users.noreply.github.com>
2025-01-07 16:01:56 +0000

    Revert "install cal-heatmap to simplify heatmap process"
    This reverts commit 86c9d236ced9dfac2395595a49625a1752e94869.

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const cal = new CalHeatmap();
-cal.paint({});
-render(<div id="cal-heatmap"></div>);
-
 var goals = 0;
+var currentday = 0;
 
-jQuery(function(){
-    $('.starterbutton').on("click", function() {
-        goals += 1;
-        alert("Adding list");
-    });
-});
-
+if (Boolean(goals > 0)) {
+    $('.starterbutton').remove();
+}
