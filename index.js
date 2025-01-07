de67cc4924b3042cadffdce17aacc1376c2f29aa
cube-king <100381926+cube-king@users.noreply.github.com>
2025-01-07 15:54:25 +0000

    commit (ten

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-import CalHeatmap from 'cal-heatmap';
-
 const cal = new CalHeatmap();
 cal.paint({});
 render(<div id="cal-heatmap"></div>);
 
 var goals = 0;
-var currentday = 0;
 
 jQuery(function(){
     $('.starterbutton').on("click", function() {
         goals += 1;
-        $('.habitlist').append('');
-    })
-    $('.deletebutton').on("click", function() {
-        $(this).parent.remove();
-    })
+        alert("Adding list");
+    });
 });
 
