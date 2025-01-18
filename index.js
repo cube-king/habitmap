0a52e646a1f43024fb489a34dd30200d48b60fa0
Jackson Pitts <100381926+cube-king@users.noreply.github.com>
2025-01-18 20:13:31 +0000

    simple js edits

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ $('.deletegoalbutton').on("click",function(){
 
 function createDays() {
     for (let step = 0; step < daysInMonth(year,month); step++) {
-        $('#graphcontainer'+containernum+'').append(
-            '<div class="day"></div>'
-        );
         elements++
+        $('#graphcontainer'+containernum+'').append(
+            '<div class="day" data-daynum="'+step+'"></div>'
+        );
     }
 }
 
@@ -37,15 +37,34 @@ function shiftDays() {
 
 function createNewGoal() {
     containernum++
-    $('.container').append(
-        '<div class="overarchingcontainer" data-containernum="'+containernum+'">',
-        '<h1 id="header'+containernum+'">Goal #'+containernum+'</h1>',
-        '<hr id="horizline'+containernum+'"><br class="break'+containernum+'">',
-        '<div class="graphcontainer" id="graphcontainer'+containernum+'"></div>',
-        '<input type="text" id="title'+containernum+'"><input type="text" id="amount'+containernum+'">',
-        '<br class="break'+containernum+'"><br class="break'+containernum+'">',
-        '</div>'
-    )
+    if (localStorage.getItem('header' + containernum) == null) {
+        $('.container').append(`
+            <div class="overarchingcontainer" data-containernum="${containernum}">
+                <h1 id="header${containernum}">Goal #${containernum}:</h1>
+                <hr id="horizline${containernum}">
+                <br class="break${containernum}">
+                <div class="graphcontainer" id="graphcontainer${containernum}"></div>
+                <input type="text" class="titleinput" id="title${containernum}">
+                <input type="text" class="amountinput" id="amount${containernum}">
+                <br class="break${containernum}">
+                <br class="break${containernum}">
+            </div>
+        `)
+    }
+    else {
+        $('.container').append(`
+            <div class="overarchingcontainer" data-containernum="${containernum}">
+                <h1 id="header${containernum}">${localStorage.getItem('header' + containernum)}</h1>
+                <hr id="horizline${containernum}">
+                <br class="break${containernum}">
+                <div class="graphcontainer" id="graphcontainer${containernum}"></div>
+                <input type="text" class="titleinput" id="title${containernum}">
+                <input type="text" class="amountinput" id="amount${containernum}">
+                <br class="break${containernum}">
+                <br class="break${containernum}">
+            </div>
+        `)
+    }
     debugTitle.textContent = containernum
     createDays()
     shiftDays()
@@ -61,18 +80,36 @@ function loadContainerNums() {
 }
 
 function deleteLastGoal() { 
-    $('#graphcontainer'+containernum+'').remove();
-    $('#title'+containernum+'').remove();
-    $('#amount'+containernum+'').remove();
-    $('#header'+containernum+'').remove();
-    $('#horizline'+containernum+'').remove();
-    const breakElements = document.getElementsByClassName('break' + containernum);
-    while (breakElements.length > 0) {
-        breakElements[0].remove();
+    if (!containernum == 0) {
+        $('#graphcontainer'+containernum+'').remove();
+        $('#title'+containernum+'').remove();
+        $('#amount'+containernum+'').remove();
+        $('#header'+containernum+'').remove();
+        $('#horizline'+containernum+'').remove();
+        const breakElements = document.getElementsByClassName('break' + containernum);
+        while (breakElements.length > 0) {
+            breakElements[0].remove();
+        }
+        localStorage.removeItem('header' + containernum);
+        console.log(localStorage.getItem('header' + containernum))
+        containernum--
+        debugTitle.textContent = containernum
+        localStorage.setItem("containernum",containernum)
     }
-    containernum--
-    debugTitle.textContent = containernum
-    localStorage.setItem("containernum",containernum)
 }
 
 loadContainerNums()
+
+document.querySelectorAll('.titleinput').forEach(ttinput => {  
+    ttinput.addEventListener('input', function() {
+        const parentContainerNum = ttinput.parentElement.getAttribute('data-containernum');
+        const header = document.getElementById('header' + parentContainerNum);
+        if (header) {
+            header.textContent = "Goal #" + parentContainerNum + ": " + ttinput.value;
+            localStorage.setItem('header' + parentContainerNum, header.textContent)
+        } else {
+            console.warn(`No header found with ID: header${parentContainerNum}`);
+        }
+        console.log('Header updated');
+    });
+});
